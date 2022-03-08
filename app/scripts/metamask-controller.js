2c272025d80dcd1e8228e5ae3b71cc95406ce4fe
David Walsh <davidwalsh83@gmail.com>
2022-03-08 11:26:54 -0600

    Change name of checkLedgerReady to more generic name

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1401,7 +1401,7 @@ export default class MetamaskController extends EventEmitter {
       establishLedgerTransportPreference: this.establishLedgerTransportPreference.bind(
         this,
       ),
-      checkLedgerReady: this.checkLedgerReady.bind(this),
+      checkDeviceReady: this.checkDeviceReady.bind(this),
 
       // qr hardware devices
       submitQRHardwareCryptoHDKey: qrHardwareKeyring.submitCryptoHDKey.bind(
@@ -2374,7 +2374,7 @@ export default class MetamaskController extends EventEmitter {
     return { ...keyState, identities };
   }
 
-  async checkLedgerReady(address) {
+  async checkDeviceReady(address) {
     const keyring = await this.keyringController.getKeyringForAccount(address);
     if (!keyring) {
       throw new Error('No keyring could be found for this address');
